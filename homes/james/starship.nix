553ab43b48f5f3ce4dbb2d773eceb2248f7e76e0
James <jamestgeiss@gmail.com>
2025-02-06 16:10:33 -0500

    Dynamic themeing for starship

--- homes/james/starship.nix
+++ homes/james/starship.nix
@@ -1,131 +1,190 @@
-{ config, ... }:
-
 {
-  programs.starship = {
-    enable = true;
-    settings = {
-      "add_newline" = false;
-      "c" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = " ";
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib) mkMerge mkIf;
+
+  tomlFormat = pkgs.formats.toml { };
+  isThemed = !(builtins.isPath config.hyprworld.wallpaper);
+
+  makeSettings = colors: {
+    "add_newline" = false;
+    "c" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = " ";
+    };
+    "character" = {
+      "disabled" = false;
+      "error_symbol" = "[](bold fg:color_red)";
+      "success_symbol" = "[](bold fg:color_green)";
+      "vimcmd_replace_one_symbol" = "[](bold fg:color_purple)";
+      "vimcmd_replace_symbol" = "[](bold fg:color_purple)";
+      "vimcmd_symbol" = "[](bold fg:color_green)";
+      "vimcmd_visual_symbol" = "[](bold fg:color_yellow)";
+    };
+    "conda" = {
+      "format" = "[[ $symbol( $environment) ](fg:#83a598 bg:color_bg3)]($style)";
+      "style" = "bg:color_bg3";
+    };
+    "directory" = {
+      "format" = "[ $path ]($style)";
+      "style" = "fg:color_fg0 bg:color_yellow";
+      "substitutions" = {
+        "Developer" = "󰲋 ";
+        "Documents" = "󰈙 ";
+        "Downloads" = " ";
+        "Music" = "󰝚 ";
+        "Pictures" = " ";
       };
-      "character" = {
-        "disabled" = false;
-        "error_symbol" = "[](bold fg:color_red)";
-        "success_symbol" = "[](bold fg:color_green)";
-        "vimcmd_replace_one_symbol" = "[](bold fg:color_purple)";
-        "vimcmd_replace_symbol" = "[](bold fg:color_purple)";
-        "vimcmd_symbol" = "[](bold fg:color_green)";
-        "vimcmd_visual_symbol" = "[](bold fg:color_yellow)";
-      };
-      "conda" = {
-        "format" = "[[ $symbol( $environment) ](fg:#83a598 bg:color_bg3)]($style)";
-        "style" = "bg:color_bg3";
-      };
-      "directory" = {
-        "format" = "[ $path ]($style)";
-        "style" = "fg:color_fg0 bg:color_yellow";
-        "substitutions" = {
-          "Developer" = "󰲋 ";
-          "Documents" = "󰈙 ";
-          "Downloads" = " ";
-          "Music" = "󰝚 ";
-          "Pictures" = " ";
-        };
-        "truncation_length" = 3;
-        "truncation_symbol" = "…/";
-      };
-      "docker_context" = {
-        "format" = "[[ $symbol( $context) ](fg:#83a598 bg:color_bg3)]($style)";
-        "style" = "bg:color_bg3";
-        "symbol" = "";
-      };
-      "format" =
-        "[](color_orange)$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda[](fg:color_bg3 bg:color_bg1)[ ](fg:color_bg1)$line_break$character";
-      "git_branch" = {
-        "format" = "[[ $symbol $branch ](fg:color_fg0 bg:color_aqua)]($style)";
-        "style" = "bg:color_aqua";
-        "symbol" = "";
-      };
-      "git_status" = {
-        "format" = "[[($all_status$ahead_behind )](fg:color_fg0 bg:color_aqua)]($style)";
-        "style" = "bg:color_aqua";
-      };
-      "golang" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = "";
-      };
-      "haskell" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = "";
-      };
-      "java" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = " ";
-      };
-      "kotlin" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = "";
-      };
-      "line_break" = {
-        "disabled" = false;
-      };
-      "nodejs" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = "";
-      };
-      "os" = {
-        "disabled" = true;
-      };
-      "palette" = "base16";
-      "palettes" = {
-        "base16" =
-          let
-            colors = config.colorScheme.palette;
-          in
-          {
-            "color_aqua" = "#${colors.base0C}";
-            "color_bg1" = "#${colors.base01}";
-            "color_bg3" = "#${colors.base03}";
-            "color_blue" = "#${colors.base0D}";
-            "color_fg0" = "#${colors.base07}";
-            "color_green" = "#${colors.base0B}";
-            "color_orange" = "#${colors.base09}";
-            "color_purple" = "#${colors.base0E}";
-            "color_red" = "#${colors.base08}";
-            "color_yellow" = "#${colors.base0A}";
-          };
-      };
-      "php" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = "";
-      };
-      "python" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = "";
-      };
-      "rust" = {
-        "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
-        "style" = "bg:color_blue";
-        "symbol" = "";
-      };
-      "time" = {
-        "disabled" = true;
-      };
-      "username" = {
-        "format" = "[ $user ]($style)";
-        "show_always" = true;
-        "style_root" = "bg:color_orange fg:color_fg0";
-        "style_user" = "bg:color_orange fg:color_fg0";
+      "truncation_length" = 3;
+      "truncation_symbol" = "…/";
+    };
+    "docker_context" = {
+      "format" = "[[ $symbol( $context) ](fg:#83a598 bg:color_bg3)]($style)";
+      "style" = "bg:color_bg3";
+      "symbol" = "";
+    };
+    "format" =
+      "[](color_orange)$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda[](fg:color_bg3 bg:color_bg1)[ ](fg:color_bg1)$line_break$character";
+    "git_branch" = {
+      "format" = "[[ $symbol $branch ](fg:color_fg0 bg:color_aqua)]($style)";
+      "style" = "bg:color_aqua";
+      "symbol" = "";
+    };
+    "git_status" = {
+      "format" = "[[($all_status$ahead_behind )](fg:color_fg0 bg:color_aqua)]($style)";
+      "style" = "bg:color_aqua";
+    };
+    "golang" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = "";
+    };
+    "haskell" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = "";
+    };
+    "java" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = " ";
+    };
+    "kotlin" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = "";
+    };
+    "line_break" = {
+      "disabled" = false;
+    };
+    "nodejs" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = "";
+    };
+    "os" = {
+      "disabled" = true;
+    };
+    "palette" = "base16";
+    "palettes" = {
+      "base16" = {
+        "color_aqua" = "#${colors.base0C}";
+        "color_bg1" = "#${colors.base01}";
+        "color_bg3" = "#${colors.base03}";
+        "color_blue" = "#${colors.base0D}";
+        "color_fg0" = "#${colors.base07}";
+        "color_green" = "#${colors.base0B}";
+        "color_orange" = "#${colors.base09}";
+        "color_purple" = "#${colors.base0E}";
+        "color_red" = "#${colors.base08}";
+        "color_yellow" = "#${colors.base0A}";
       };
     };
+    "php" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = "";
+    };
+    "python" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = "";
+    };
+    "rust" = {
+      "format" = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+      "style" = "bg:color_blue";
+      "symbol" = "";
+    };
+    "time" = {
+      "disabled" = true;
+    };
+    "username" = {
+      "format" = "[ $user ]($style)";
+      "show_always" = true;
+      "style_root" = "bg:color_orange fg:color_fg0";
+      "style_user" = "bg:color_orange fg:color_fg0";
+    };
   };
+
+  nix-colors-lib = inputs.nix-colors.lib.contrib { inherit pkgs; };
+in
+{
+  config = mkMerge [
+    (mkIf (!isThemed) {
+      programs.starship = {
+        enable = true;
+        settings = mkIf (!isThemed) (makeSettings config.colorScheme.palette);
+      };
+    })
+    (mkIf isThemed {
+      home.packages = [ config.programs.starship.package ];
+
+      programs.bash.initExtra = ''
+        if [[ $TERM != "dumb" ]]; then
+          if [[ -v WAYLAND_DISPLAY ]]; then
+            mode="$(darkman get)"
+
+            if [ "$mode" = "dark" ]; then
+              export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/dark.toml
+            elif [ "$mode" = "light" ]; then
+              export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/light.toml
+            fi
+          fi
+
+          eval "$(${config.home.profileDirectory}/bin/starship init bash --print-full-init)"
+        fi
+      '';
+
+      xdg.configFile = {
+        "starship.toml".source = tomlFormat.generate "startship-config" (
+          makeSettings config.colorScheme.palette
+        );
+
+        "starship/dark.toml".source = tomlFormat.generate "startship-config-dark" (
+          makeSettings (
+            (nix-colors-lib.colorSchemeFromPicture {
+              path = config.hyprworld.wallpaper.dark;
+              variant = "dark";
+            }).palette
+          )
+        );
+
+        "starship/light.toml".source = tomlFormat.generate "startship-config-light" (
+          makeSettings (
+            (nix-colors-lib.colorSchemeFromPicture {
+              path = config.hyprworld.wallpaper.light;
+              variant = "dark"; # Intentional for better contrast
+            }).palette
+          )
+        );
+      };
+    })
+  ];
 }
