8ade0823ecdaae2ac70375cda6aa4e849732dcc1
James <jamestgeiss@gmail.com>
2025-03-05 12:24:56 -0500

    Remove dead code

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -10,7 +10,6 @@ let
   inherit (lib)
     mkEnableOption
     mkIf
-    mkMerge
     mkOption
     types
     ;
@@ -21,12 +20,6 @@ in
 
   options.tetrago.hyprland = {
     enable = mkEnableOption "Hyprland.";
-
-    homeManagerModule = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Adds the home manager module and sets the appropriate package";
-    };
 
     session = mkOption {
       type = types.bool;
@@ -39,36 +32,23 @@ in
     let
       cfg = config.tetrago.hyprland;
     in
-    mkIf cfg.enable (mkMerge [
-      {
-        nix.settings = {
-          substituters = [ "https://hyprland.cachix.org" ];
-          trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-        };
+    mkIf cfg.enable {
+      nix.settings = {
+        substituters = [ "https://hyprland.cachix.org" ];
+        trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      };
 
-        programs.hyprland = {
-          enable = true;
-          package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-          portalPackage =
-            inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-        };
+      programs.hyprland = {
+        enable = true;
+        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        portalPackage =
+          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      };
 
-        services.xserver.displayManager.session = optional cfg.session {
-          manage = "desktop";
-          name = "Hyprland";
-          start = "exec Hyprland 1>/dev/null 2>/dev/null";
-        };
-      }
-      (mkIf cfg.homeManagerModule {
-        home-manager.sharedModules = [
-          (
-            { inputs, pkgs, ... }:
-            {
-              wayland.windowManager.hyprland.package =
-                inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-            }
-          )
-        ];
-      })
-    ]);
+      services.xserver.displayManager.session = optional cfg.session {
+        manage = "desktop";
+        name = "Hyprland";
+        start = "exec Hyprland 1>/dev/null 2>/dev/null";
+      };
+    };
 }
