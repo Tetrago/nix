fd1ed562751574bcae27ca23880bd3ec0df8be14
James <jamestgeiss@gmail.com>
2025-03-14 13:06:00 -0400

    Fix system Hyprland

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -26,8 +26,11 @@ in
         trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
       };
 
-      services.displayManager.sessionPackages = [
-        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
-      ];
+      programs.hyprland = {
+        enable = true;
+        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        portalPackage =
+          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      };
     };
 }
