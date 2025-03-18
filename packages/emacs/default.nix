6c185059993614dba351cc4c506a004895fbd154
James <jamestgeiss@gmail.com>
2025-03-17 20:27:05 -0400

    Emacs updates

--- packages/emacs/default.nix
+++ packages/emacs/default.nix
@@ -20,16 +20,37 @@ let
         defaultInitFile = true;
 
         extraEmacsPackages =
+          let
+            maple-minibuffer = pkgs.emacsPackages.trivialBuild rec {
+              pname = "maple-minibuffer";
+              version = "2021-10-16";
+
+              src = pkgs.fetchFromGitHub {
+                owner = "honmaple";
+                repo = "emacs-${pname}";
+                rev = "21a62e10c2c721d772e1fcf5de6aa4166baaea56";
+                hash = "sha256-8LhpQqJwmlMyu74h6Zd0G+Tjyqm3sJCBet0MBzJtamg=";
+              };
+            };
+          in
           epkgs: with epkgs; [
             calt
             f
             goto-chg
             ht
+            language-id
+            llama
             lsp
             lv
+            maple-minibuffer
             markdown-mode
             s
             spinner
+            wgrep
+            pkgs.clang-tools
+            pkgs.nixfmt-rfc-style
+            pkgs.ripgrep
+            pkgs.rustfmt
           ];
       };
 in
