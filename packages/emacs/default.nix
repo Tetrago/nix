b521582682c5f2614880d845f38efd397b48d49c
James <jamestgeiss@gmail.com>
2025-03-19 00:31:01 -0400

    Emacs improvements

--- packages/emacs/default.nix
+++ packages/emacs/default.nix
@@ -20,19 +20,6 @@ let
         defaultInitFile = true;
 
         extraEmacsPackages =
-          let
-            maple-minibuffer = pkgs.emacsPackages.trivialBuild rec {
-              pname = "maple-minibuffer";
-              version = "2021-10-16";
-
-              src = pkgs.fetchFromGitHub {
-                owner = "honmaple";
-                repo = "emacs-${pname}";
-                rev = "21a62e10c2c721d772e1fcf5de6aa4166baaea56";
-                hash = "sha256-8LhpQqJwmlMyu74h6Zd0G+Tjyqm3sJCBet0MBzJtamg=";
-              };
-            };
-          in
           epkgs: with epkgs; [
             calt
             f
@@ -42,7 +29,6 @@ let
             llama
             lsp
             lv
-            maple-minibuffer
             markdown-mode
             s
             spinner
@@ -52,6 +38,10 @@ let
             pkgs.ripgrep
             pkgs.rustfmt
           ];
+
+        override = final: prev: {
+          ultra-scroll = pkgs.callPackage ./ultra-scroll.nix { };
+        };
       };
 in
 pkgs.stdenvNoCC.mkDerivation {
---
+++ packages/emacs/ultra-scroll.nix
@@ -0,0 +1,16 @@
+{
+  fetchFromGitHub,
+  emacsPackages,
+}:
+
+emacsPackages.trivialBuild rec {
+  pname = "ultra-scroll";
+  version = "2025-03-12";
+
+  src = fetchFromGitHub {
+    owner = "jdtsmith";
+    repo = pname;
+    rev = "b72c507f6702db18d971a6b6bdc692e260f21159";
+    hash = "sha256-AehGNc8ll/Q0HVp1OCj1bVBuXNh0Y43kp5EteSpwvmQ=";
+  };
+}
