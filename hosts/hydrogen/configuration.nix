5bba972ac90814263f2ca4058e7d255fa50ee946
James <jamestgeiss@gmail.com>
2025-04-17 16:03:54 -0400

    Improve scrolling accuracy

--- hosts/hydrogen/configuration.nix
+++ hosts/hydrogen/configuration.nix
@@ -130,7 +130,7 @@
         ---
         - Rule:
           - Setting: [9DBC514C, scroll-ratchet, 1]
-          - Execute: ['${config.wayland.windowManager.hyprland.package}/bin/hyprctl', keyword, 'input:scroll_factor', '0.1']
+          - Execute: ['${config.wayland.windowManager.hyprland.package}/bin/hyprctl', keyword, 'input:scroll_factor', '0.05']
           - Execute: ['${config.wayland.windowManager.hyprland.package}/bin/hyprctl', keyword, 'input:emulate_discrete_scroll', '0']
           - Set: [9DBC514C, hires-smooth-resolution, true]
         - Rule:
