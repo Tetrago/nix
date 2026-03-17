90d19fbd30302dee0a44fcc559d85389b2a1c29e
James <jamestgeiss@gmail.com>
2026-03-17 12:37:17 -0400

    patch: orca-slicer

--- hosts/hydrogen/homes.nix
+++ hosts/hydrogen/homes.nix
@@ -34,7 +34,40 @@
 
       home.packages = with pkgs; [
         onshape
-        orca-slicer
+        (
+          (orca-slicer.override {
+            wxGTK31 = (pkgs.wxGTK31.override { withEGL = false; }).overrideAttrs (
+              final: prev: {
+                buildInputs = (prev.buildInputs or [ ]) ++ [ pkgs.libsecret ];
+              }
+            );
+          }).overrideAttrs
+          (
+            final: prev: {
+              version = "zaa";
+
+              src = fetchFromGitHub {
+                owner = "OrcaSlicer";
+                repo = "OrcaSlicer";
+                rev = "4c493dd0d397ea1cecdd54eccb4e5db35c087306";
+                hash = "sha256-vqxeYUOeUaR1Nir251hFPaXFrznlgMfm17dQdN1/nSo=";
+              };
+
+              buildInputs = (prev.buildInputs or [ ]) ++ [
+                pkgs.draco
+                pkgs.opencv
+              ];
+
+              cmakeFlags = (prev.cmakeFlags or [ ]) ++ [
+                (lib.cmakeFeature "LIBNOISE_LIBRARY_RELEASE" "${pkgs.libnoise}/lib/libnoise-static.a")
+              ];
+
+              patches =
+                builtins.filter (p: !(builtins.match ".*world.*" (toString p) != null)) (prev.patches or [ ])
+                ++ [ ./orca-slicer.patch ];
+            }
+          )
+        )
       ];
     };
 }
